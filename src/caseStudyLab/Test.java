9abe172d8d8a53596c7fd0e22befda2285101d28
Sarah <SarahLammert@stmarksschool.org>
2019-09-30 12:13:23 -0400

    Update Test.java

--- src/caseStudyLab/Test.java
+++ src/caseStudyLab/Test.java
@@ -5,7 +5,7 @@ import java.awt.Color;
 public class Test {
 
 	public static void main(String[] args) {
-		Balloon b = new Balloon(100, 100, 20, Color.RED);
+		Balloon b = new FancyBalloon(100, 100, 20, Color.RED);
 		System.out.println(b);
 		//It prints out caseStudyLab.Balloon@72ea2f77 -- location?
 
